8f596d579f8cd652799798239f393cb3eaccdb81
Bruno Risso <brunorisso58@gmail.com>
2023-04-21 18:44:56 -0300

    Creating and configurating services

---
+++ LumenApiGateway/config/services.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'brands' => [
+        'base_uri' => env('BRANDS_SERVICE_BASE_URL')
+    ],
+    'cities' => [
+        'base_uri' => env('CITIES_SERVICE_BASE_URL')
+    ],
+    'cars' => [
+        'base_uri' => env('CARS_SERVICE_BASE_URL')
+    ],
+];
