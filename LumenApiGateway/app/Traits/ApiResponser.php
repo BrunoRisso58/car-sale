e52be56368180c3f8c28dd4a63f862cae838041b
Bruno Risso <brunorisso58@gmail.com>
2023-04-21 18:07:27 -0300

    Creating ApiResponser trait

---
+++ LumenApiGateway/app/Traits/ApiResponser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser {
+
+    /**
+     * Return a success response
+     * @param array|string $data
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data, $code = Response::HTTP_OK) {
+        return response()->json($data, $code)->header('Content-Type', 'application/json');
+    }
+
+    /**
+     * Return an error response
+     * @param array|string $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code) {
+        return response()->json(['error' => $message, 'code' => $code], $code)->header('Content-Type', 'application/json');
+    }
+
+    /**
+     * Return the error message
+     * @param array|string $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function errorMessage($message, $code) {
+        return response($message, $code)->header('Content-Type', 'application/json');
+    }
+
+}
