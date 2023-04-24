58f2a38d365c07e74775d9b24c7d80cb8b5bef2f
Bruno Risso <brunorisso58@gmail.com>
2023-04-24 10:59:30 -0300

    Handleing exceptions

--- LumenApiGateway/app/Exceptions/Handler.php
+++ LumenApiGateway/app/Exceptions/Handler.php
@@ -3,14 +3,20 @@
 namespace App\Exceptions;
 
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
+use GuzzleHttp\Exception\ClientException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +55,42 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpException) {
+            $code = $exception->getCode();
+            $message = Response::$statusTexts[$code];
+            return $this->errorResponse($message, $code);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("There is not any instance of {$model} with the given id", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            $message = $exception->getMessage();
+            return $this->errorResponse($message, Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            $message = $exception->getMessage();
+            return $this->errorResponse($message, Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($exception instanceof ClientException) {
+            $message = $exception->getResponse()->getBody();
+            $code = $exception->getCode();
+            return $this->errorMessage($message, $code);
+        }
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected error. Try later.', Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
